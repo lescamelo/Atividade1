302cce39ff67426421352deb87e7609dd5d3d9a2
Valesca Melo <valesca.melo@academico.ifpb.edu.br>
2021-03-03 22:19:00 -0300

    Questão 2

---
+++ quest2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Diretório 1: " diretorio_1
+read -p "Diretório 2: " diretorio_2
+read -p "Diretório 3: " diretorio_2
+
+ls ${diretorio_1}
+ls ${diretorio_2}
+ls ${diretorio_3}
